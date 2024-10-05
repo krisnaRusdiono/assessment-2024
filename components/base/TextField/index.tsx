4094bcce07b1a036f5c887af9079e08bfff192e7
Krisna Rusdiono <krisnarusdiono@gmail.com>
2024-10-05 16:12:05 +0700

    feat: add newsletter components

---
+++ components/base/TextField/index.tsx
@@ -0,0 +1,5 @@
+import { TextField as MUITextField, TextFieldProps } from '@mui/material'
+
+const TextField = (props: TextFieldProps) => <MUITextField {...props} />
+
+export default TextField
--- views/DetailAlbum/DetailAlbum.tsx
+++ views/DetailAlbum/DetailAlbum.tsx
@@ -1,12 +1,14 @@
 import AlbumCover from './components/AlbumCover'
 import TrackList from './components/TrackList'
 import OtherRelease from './components/OtherRelease'
+import NewsLetter from './components/NewsLetter'
 
 const DetailAlbum = () => (
   <>
     <AlbumCover />
     <TrackList />
     <OtherRelease />
+    <NewsLetter />
   </>
 )
 
