e084e2fa98613476ea305e1db34c221374658196
Krisna Rusdiono <krisnarusdiono@gmail.com>
2024-10-05 11:39:47 +0700

    feat: add enhancement for responsiveness in footer

--- components/ui/Footer/index.tsx
+++ components/ui/Footer/index.tsx
@@ -1,22 +1,25 @@
 import Typography from '@/components/base/Typography'
 
 const Footer = () => (
-  <div className="bg-white flex justify-center gap-8 p-8">
-    <div className="w-full box-border flex gap-10 justify-end">
+  <div className="bg-white flex justify-center gap-8 p-8 flex-wrap md:flex-nowrap">
+    <div className="w-full box-border flex flex-wrap lg:flex-nowrap gap-4 lg:gap-10 justify-center md:justify-end">
       <Typography
         variant="body1"
         className="text-2xl font-semibold text-center"
       >
         TRADE
       </Typography>
-      <div className="flex flex-col gap-4 py-3 max-w-120">
+      <div className="flex flex-col gap-4 py-3 max-w-120 items-center md:items-end lg:items-start">
         <Typography variant="body2" className="font-semibold">
           Latest Blog Post
         </Typography>
         <Typography variant="h6" className="text-2xl">
           Ready to get started?
         </Typography>
-        <Typography variant="body2">
+        <Typography
+          variant="body2"
+          className="lg:text-start md:text-end text-center"
+        >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -24,9 +27,9 @@ const Footer = () => (
         </Typography>
       </div>
     </div>
-    <div className="border border-neutral-500 border-solid" />
-    <div className="w-full box-border flex gap-10">
-      <div className="font-semibold py-3 flex flex-col gap-4">
+    <div className="border border-neutral-500 border-solid w-full md:w-0" />
+    <div className="w-full box-border flex gap-10 justify-center md:justify-start">
+      <div className="font-semibold py-3 flex flex-col gap-4 items-center md:items-start md:justify-between">
         <Typography variant="body2" className="font-semibold">
           Product
         </Typography>
@@ -39,7 +42,7 @@ const Footer = () => (
         </div>
         <Typography variant="body2">© 2010 — 2020</Typography>
       </div>
-      <div className="font-semibold py-3 flex flex-col gap-4">
+      <div className="font-semibold py-3 flex flex-col gap-4 items-center md:items-start md:justify-between">
         <Typography variant="body2" className="font-semibold">
           Company
         </Typography>
